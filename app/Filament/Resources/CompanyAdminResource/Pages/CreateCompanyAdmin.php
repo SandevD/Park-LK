811fa94e8bb72ab93be0c9ca2d18a828d7dd2828
Sandev Dullewa <111951766+SandevD@users.noreply.github.com>
2025-02-04 17:39:12 +0530

    ✨  Add CreateCompanyAdmin page for CompanyAdminResource in Filament

---
+++ app/Filament/Resources/CompanyAdminResource/Pages/CreateCompanyAdmin.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\CompanyAdminResource\Pages;
+
+use App\Filament\Resources\CompanyAdminResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateCompanyAdmin extends CreateRecord
+{
+    protected static string $resource = CompanyAdminResource::class;
+}
