5a9b5edb98e4ea892e89c22e15f40502138beb89
Sandev Dullewa <111951766+SandevD@users.noreply.github.com>
2025-02-04 15:14:32 +0530

    🎨  Added fillable properties and relationships to Booking model

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Booking extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'parking_location_id',
+        'start_date',
+        'end_date',
+    ];
+
+    // Relationships
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function parkingLocation()
+    {
+        return $this->belongsTo(ParkingLocation::class);
+    }
 }
