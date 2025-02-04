d3261eac0487829a2c7233abb29c1efc3ad6a059
Sandev Dullewa <111951766+SandevD@users.noreply.github.com>
2025-02-04 15:14:39 +0530

    🎨  Added fillable properties and relationships to Company model

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'city_id',
+    ];
+
+    // Relationships
+
+    public function city()
+    {
+        return $this->belongsTo(City::class);
+    }
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
 }
